a618dfe8acf8bb45178b1d829bc54262056729ef
zxy <xiangyang.zhao@voiladev.xyz>
2024-04-10 16:42:57 +0800

    feat:新增配置

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -1,11 +1,56 @@
-import { Button } from 'antd';
+import { Button, Checkbox, Form, Input} from 'antd';
 import './index.less';
 function Home() {
   console.log(import.meta.env.MODE)
+  const onFinish = (values: unknown) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: unknown) => {
+    console.log('Failed:', errorInfo);
+  };
   return (
     <>
-      <div className='red'>home1</div>
-      <Button type="primary">Button</Button>
+      <Form
+      name="basic"
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+      style={{ maxWidth: 600 }}
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input placeholder="input placeholder"/>
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
     </>
   )
 }
